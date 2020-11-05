22f14d0e222c705788d7c621eb2a165332c900e4
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-11-05 22:57:44 +0900

    wAssetsMUI 리액트로 개발예정

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './menu/menu.js';
+
+class App extends React.Component { 
+    render(){
+        return (
+        <div>
+            <Menu/>           
+        </div>
+        );
+    }
+}
+export default App;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Menu extends React.Component { 
+    render(){
+        return (
+        <div>
+            자산관리 | 계좌관리
+
+        </div>
+        );
+    }
+}
+export default Menu;
